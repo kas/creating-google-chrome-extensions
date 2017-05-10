0b6d4ca21fde7cec4fa97b575ea307783719931a
Kenneth Schnall <kas6570@psu.edu>
2017-05-10 06:30:45 -0400

    Add chapter 2 HelloPageAction, HelloShortcutKey, and PageActionNotes Chrome Extensions

---
+++ 02/HelloPageAction/event_script.js
@@ -0,0 +1,36 @@
+//region {variables and functions}
+var consoleGreeting = "Hello World! - from event_script.js";
+var queryInfoForAllTabs = {
+  //"active":false,"currentWindow":true
+};
+function logTabs(tabs) {
+  console.group('Tabs');
+  console.log(tabs);
+  console.groupEnd('Tabs');
+}
+function queryTabsAndShowPageActions() {
+  chrome.tabs.query(
+    queryInforForAllTabs,
+    function(tabs) {
+      console.log('All tabs length: %s', tabs.length);
+      //Output tabs object to the console as a separate visual group
+      logTabs(tabs);
+      if (tabs.length > 0) {
+        for (var i = 0; i < tabs.length; i++) {
+          chrome.pageAction.show(tabs[i].id);
+        }
+      }
+    }
+  );
+}
+//end-region
+
+//region {calls}
+console.log(consoleGreeting);
+//Show Page-Actions using the chrome.tabs.query method
+//queryTabsAndShowPageActions();
+//Show Page-Actions using the onUpdated event
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  chrome.pageAction.show(tabId);
+});
+//end-region
